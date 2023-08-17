f3877beabafe76328c22bf2a53ecf7e92190e6e8
Katherine Hambley <superdesigngirl@mac.com>
2023-08-17 18:28:09 -0500

    Added header component

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LittleLemonHeader from './components/LittleLemonHeader';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Little Lemon App!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#495E57',
+      }}>
+      <LittleLemonHeader />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/LittleLemonHeader.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+
+export default function LittleLemonHeader() {
+  return (
+    <View style={{ 
+        flex: 0.11, 
+        backgroundColor: '#F4CE14' }}>
+      <Text
+        style={{
+          padding: 50,
+          fontSize: 30,
+          color: 'black',
+          textAlign: 'center',
+        }}>
+        Little Lemon
+      </Text>
+    </View>
+  );
+}
