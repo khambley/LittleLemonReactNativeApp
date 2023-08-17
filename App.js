76af71a90039e69a7805e4c8a3622f63f4c09c28
Katherine Hambley <superdesigngirl@mac.com>
2023-08-17 18:43:19 -0500

    Added footer component

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
 
 
 export default function App() {
   return (
-    <View
+    //shorthand for react's new "Fragment" element
+    //https://legacy.reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
+    <>
+      <View
       style={{
         flex: 1,
         backgroundColor: '#495E57',
       }}>
-      <LittleLemonHeader />
-    </View>
+        <LittleLemonHeader />
+      </View>
+      <View style={{ 
+        backgroundColor: '#495E57' 
+      }}>
+        <LittleLemonFooter />
+      </View>
+
+    </>
+    
+  
   );
 }
