04a8c9f554659db48a2cfccecfbbf9e75d0b3d5d
Katherine Hambley <superdesigngirl@mac.com>
2023-08-23 16:53:16 -0500

    Week 1.4 - Scrollview Example

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 
 export default function App() {
@@ -16,7 +17,8 @@ export default function App() {
         backgroundColor: '#495E57',
       }}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        {/* <WelcomeScreen /> */}
+        <MenuItems />
       </View>
       <View style={{ 
         backgroundColor: '#495E57' 
