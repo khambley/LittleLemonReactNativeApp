2e746d2d5e64091082820487dfd11eb51c52722e
Katherine Hambley <superdesigngirl@mac.com>
2023-08-23 16:29:32 -0500

    Week 1.4 - Added Welcome Screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './WelcomeScreen';
 
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         backgroundColor: '#495E57',
       }}>
         <LittleLemonHeader />
+        <WelcomeScreen />
       </View>
       <View style={{ 
         backgroundColor: '#495E57' 
--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -2,12 +2,10 @@ import { View, Text } from 'react-native';
 
 export default function LittleLemonHeader() {
   return (
-    <View style={{ 
-        flex: 0.11, 
-        backgroundColor: '#F4CE14' }}>
+    <View style={{ backgroundColor: '#F4CE14' }}>
       <Text
         style={{
-          padding: 50,
+          paddingTop: 55,
           fontSize: 30,
           color: 'black',
           textAlign: 'center',
