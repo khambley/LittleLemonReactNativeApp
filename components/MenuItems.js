38f7ce2684265b2db1df9df7bcb647845e5672d2
Katherine Hambley <superdesigngirl@mac.com>
2023-08-28 19:37:31 -0500

    Week 2.2.5 - Added SectionList component

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,34 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList, SectionList } from 'react-native';
 
 // const menuItemsToDisplay = [
 //   'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
 // ];
 
-const menuItemsToDisplay = [
-  { name: 'Hummus', price: '$5.00', id: '1A' },
-  { name: 'Moutabal', price: '$5.00', id: '2B' },
-  { name: 'Falafel', price: '$7.50', id: '3C' },
-  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-  { name: 'Kofta', price: '$5.00', id: '5E' },
-  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-  { name: 'Fries', price: '$3.00', id: '11K' },
-  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-  { name: 'Baklava', price: '$3.00', id: '18S' },
-  { name: 'Tartufo', price: '$3.00', id: '19T' },
-  { name: 'Tiramisu', price: '$5.00', id: '20U' },
-  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+// const menuItemsToDisplay = [
+//   { name: 'Hummus', price: '$5.00', id: '1A' },
+//   { name: 'Moutabal', price: '$5.00', id: '2B' },
+//   { name: 'Falafel', price: '$7.50', id: '3C' },
+//   { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+//   { name: 'Kofta', price: '$5.00', id: '5E' },
+//   { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+//   { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+//   { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+//   { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+//   { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+//   { name: 'Fries', price: '$3.00', id: '11K' },
+//   { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+//   { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+//   { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+//   { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+//   { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+//   { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+//   { name: 'Baklava', price: '$3.00', id: '18S' },
+//   { name: 'Tartufo', price: '$3.00', id: '19T' },
+//   { name: 'Tiramisu', price: '$5.00', id: '20U' },
+//   { name: 'Panna Cotta', price: '$5.00', id: '21V' },
 
-];
+// ];
+const menuItemsToDisplay = [
+    {
+      title: 'Appetizers',
+      data: [
+        { name: 'Hummus', price: '$5.00' },
+        { name: 'Moutabal', price: '$5.00' },
+        { name: 'Falafel', price: '$7.50' },
+        { name: 'Marinated Olives', price: '$5.00' },
+        { name: 'Kofta', price: '$5.00' },
+        { name: 'Eggplant Salad', price: '$8.50' },
+      ],
+    },
+    {
+      title: 'Main Dishes',
+      data: [
+        { name: 'Lentil Burger', price: '$10.00' },
+        { name: 'Smoked Salmon', price: '$14.00' },
+        { name: 'Kofta Burger', price: '$11.00' },
+        { name: 'Turkish Kebab', price: '$15.50' },
+      ],
+    },
+    {
+      title: 'Sides',
+      data: [
+        { name: 'Fries', price: '$3.00', id: '11K' },
+        { name: 'Buttered Rice', price: '$3.00' },
+        { name: 'Bread Sticks', price: '$3.00' },
+        { name: 'Pita Pocket', price: '$3.00' },
+        { name: 'Lentil Soup', price: '$3.75' },
+        { name: 'Greek Salad', price: '$6.00' },
+        { name: 'Rice Pilaf', price: '$4.00' },
+      ],
+    },
+    {
+      title: 'Desserts',
+      data: [
+        { name: 'Baklava', price: '$3.00' },
+        { name: 'Tartufo', price: '$3.00' },
+        { name: 'Tiramisu', price: '$5.00' },
+        { name: 'Panna Cotta', price: '$5.00' },
+      ],
+    },
+  ];
 const Separator = () => <View style={menuStyles.separator} />
 
 const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>
@@ -50,6 +93,10 @@ const MenuItems = () => {
 
     const renderItem = ({ item }) => <Item name={item.name} price={item.price} />
 
+    const renderSectionHeader = ({ section: {title} }) => (
+        <Text style={menuStyles.sectionHeader}>{title}</Text>
+    );
+
     return (
         <View style={menuStyles.container}>
             {/* <ScrollView
@@ -63,12 +110,16 @@ const MenuItems = () => {
                 </Text>
             </ScrollView> */}
             {/* <Text style={menuStyles.headerText}>View Menu</Text> */}
-            <FlatList data={menuItemsToDisplay} 
+            {/* <FlatList data={menuItemsToDisplay} 
                       renderItem={renderItem} 
                       keyExtractor={(item) => item.id}
                       ItemSeparatorComponent={Separator}
                       ListHeaderComponent={Header}
-                      ListFooterComponent={Footer} />
+                      ListFooterComponent={Footer} /> */}
+            <SectionList keyExtractor={(item, index) => item + index }
+                         sections={menuItemsToDisplay}
+                         renderItem={renderItem}
+                         renderSectionHeader={renderSectionHeader} />
         </View>
     );
 };
@@ -98,5 +149,11 @@ const menuStyles = StyleSheet.create({
     separator: {
         borderBottomWidth: 1,
         borderColor: '#EDEFEE'
+    },
+    sectionHeader: {
+        backgroundColor: '#F4CE14',
+        textAlign: 'center',
+        fontSize: 25,
+        fontWeight: 200
     }
 })
