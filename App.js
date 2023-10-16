c289f663bfee2234da6d87a0ef4d7ea6d4100744
Katherine Hambley <superdesigngirl@mac.com>
2023-10-16 09:39:18 -0500

    4.1.11 Added Stack navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
@@ -10,27 +11,39 @@ import MyTextComponent from './components/MyTextComponent';
 import MenuItems from './components/MenuItems';
 import ViewBoxesWithColorAndText from './ViewBoxesWithColorAndText';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator 
+      initialRouteName="Menu"
+      screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>
+        <Stack.Screen 
+        name="Welcome" 
+        component={WelcomeScreen}
+        options={{ title: 'Home' }} />
+        <Stack.Screen name="Menu" component={MenuItems} />
+      </Stack.Navigator>
+    </NavigationContainer>
     //shorthand for react's new "Fragment" element
     //https://legacy.reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
-    <NavigationContainer>
-       <>
-      <View
-      style={styles.container}>
-        <LittleLemonHeader />
-        {/* <ViewBoxesWithColorAndText /> */}
-        <WelcomeScreen />
-        {/* <MenuItems /> */}
-        {/* <LoginScreen /> */}
-        {/* <MyTextComponent /> */}
-      </View>
-      <View style={styles.footer}>
-        <LittleLemonFooter />
-      </View>
-    </>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //    <>
+    //   <View
+    //   style={styles.container}>
+    //     <LittleLemonHeader />
+    //     {/* <ViewBoxesWithColorAndText /> */}
+    //     <WelcomeScreen />
+    //     {/* <MenuItems /> */}
+    //     {/* <LoginScreen /> */}
+    //     {/* <MyTextComponent /> */}
+    //   </View>
+    //   <View style={styles.footer}>
+    //     <LittleLemonFooter />
+    //   </View>
+    // </>
+    // </NavigationContainer>
    
   );
 }
